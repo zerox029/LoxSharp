f0257a071ebbf93f933ca306f163ab7599dc71bc
Étienne Plante <zerox029@gmail.com>
2021-10-23 01:03:16 -0400

    Added base framework

---
+++ Lox/Lox.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Lox {
+  class Lox {
+    static Boolean hadError = false;
+
+    static void Main(string[] args) {
+      if (args.Length > 1) {
+        Console.WriteLine("Usage: loxsharp [script]");
+        System.Environment.Exit(1);
+      } else if (args.Length == 1) {
+        RunFile(args[0]);
+      } else {
+        RunPrompt();
+      }
+    }
+
+    private static void RunFile(string path) {
+      byte[] bytes = File.ReadAllBytes(path);
+      Run(new string(bytes));
+
+      if (hadError) 
+        System.Environment.Exit(1);
+    }
+    
+    private static void RunPrompt() {
+      ///TODO
+    }
+
+    private static void Run(string source) {
+      Scanner scanner = new Scanner();
+      List<Token> tokens = scanner.ScanTokens();
+
+      foreach (Token token in tokens) {
+        Console.WriteLine(token);
+      }
+    }
+
+    public static void Error(int line, string message) {
+      Report(line, "", message);
+    }
+
+    private static void Report(int line, string where, string message) {
+      Console.Error("[line " + line + "] Error" + where + ": " + message);
+      hadError = true;
+    }
+  }
+}
